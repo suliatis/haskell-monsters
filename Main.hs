ec4b5c4cf478a79dcb252c9f77f05c84d2fe696a
Attila Süli <suli.zakar.attila@gmail.com>
2020-08-23 12:34:01 +0200

    Move around the player

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import           Graphics.Gloss.Interface.Pure.Game (Color, Display (FullScreen), Event, Picture, blue, color, pictures,
+import           Graphics.Gloss.Interface.Pure.Game (Color, Display (FullScreen), Event (EventKey), Key (..),
+                                                     KeyState (..), Picture, SpecialKey (..), blue, color, pictures,
                                                      play, rectangleSolid, translate, white)
 
 class Initialize a where
@@ -19,14 +20,63 @@ instance Draw Placeholder where
   draw (Placeholder (Point x y) (Size a)) =
     translate x y $ rectangleSolid a a
 
-data Player = Player Placeholder
+newtype Speed = Speed Float
+
+defaultPlayerSpeed :: Speed
+defaultPlayerSpeed = Speed 10
+
+data Direction
+  = Direction'Left
+  | Direction'Down
+  | Direction'Up
+  | Direction'Right
+
+movePoint :: Direction -> Speed -> Point -> Point
+movePoint direction (Speed speed) (Point x y) =
+  case direction of
+    Direction'Left  -> Point (x - speed) y
+    Direction'Down  -> Point x (y - speed)
+    Direction'Up    -> Point x (y + speed)
+    Direction'Right -> Point (x + speed) y
+
+data MoveOrStay
+  = Move Direction Speed
+  | Stay
+
+moveOrStay :: Key -> KeyState -> MoveOrStay -> Speed -> MoveOrStay
+moveOrStay (SpecialKey KeyLeft) Down _ speed                   = Move Direction'Left speed
+moveOrStay (SpecialKey KeyLeft) Up (Move Direction'Left _) _   = Stay
+moveOrStay (SpecialKey KeyDown) Down _ speed                   = Move Direction'Down speed
+moveOrStay (SpecialKey KeyDown) Up (Move Direction'Down _) _   = Stay
+moveOrStay (SpecialKey KeyUp) Down _ speed                     = Move Direction'Up speed
+moveOrStay (SpecialKey KeyUp) Up (Move Direction'Up _) _       = Stay
+moveOrStay (SpecialKey KeyRight) Down _ speed                  = Move Direction'Right speed
+moveOrStay (SpecialKey KeyRight) Up (Move Direction'Right _) _ = Stay
+moveOrStay _ _ move _                                          = move
+
+data Player = Player Placeholder MoveOrStay
 
 instance Initialize Player where
-  initialize = Player (Placeholder (Point 0 0) (Size 20))
+  initialize = Player (Placeholder (Point 0 0) (Size 20)) Stay
 
 instance Draw Player where
-  draw (Player placeholder) =
+  draw (Player placeholder _) =
     color blue $ draw placeholder
+
+movePlayer :: Player -> Player
+movePlayer (Player (Placeholder point size) (Move direction speed)) =
+  let
+    point' = movePoint direction speed point
+  in
+    Player (Placeholder point' size) (Move direction speed)
+movePlayer player = player
+
+updatePlayerToMoveOrStay :: Key -> KeyState -> Player -> Player
+updatePlayerToMoveOrStay key state (Player placeholder move) = Player placeholder $ moveOrStay key state move defaultPlayerSpeed
+
+updatePlayer :: Event -> Player -> Player
+updatePlayer (EventKey key state _ _) player = updatePlayerToMoveOrStay key state player
+updatePlayer _ player                        = player
 
 data World = World Player
 
@@ -37,10 +87,6 @@ instance Draw World where
   draw (World player) =
     pictures $ [ draw player ]
 
-main :: IO ()
-main =
-  play FullScreen background frameRate initialize draw update tick
-
 background :: Color
 background = white
 
@@ -48,7 +94,12 @@ frameRate :: Int
 frameRate = 60
 
 update :: Event -> World -> World
-update _ world = world
+update  event (World player) = World $ updatePlayer event player
 
 tick :: Float -> World -> World
-tick _ world = world
+tick _ (World player) = World $ movePlayer player
+
+main :: IO ()
+main =
+  play FullScreen background frameRate initialize draw update tick
+
